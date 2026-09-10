aa30033e1597dfe92af4f6413cc8973df403118e
Vicky <seomynds@gmail.com>
2026-09-10 07:46:59 +0530

    Fix email confirmation redirect

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -24,7 +24,7 @@ export default function SignupPage() {
       email,
       password,
       options: {
-        emailRedirectTo: `${window.location.origin}/auth/callback`,
+        emailRedirectTo: `${window.location.origin}/login?confirmed=true`,
       },
     });
 
